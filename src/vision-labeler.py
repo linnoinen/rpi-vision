e0e18b3290976b6e966ab1b37fc20e253c331817
Karri Linnoinen <karri.linnoinen@gmail.com>
2018-10-01 19:41:50 +0300

    init

---
+++ src/vision-labeler.py
@@ -0,0 +1,38 @@
+import argparse
+import base64
+import picamera
+import json
+
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+
+def takephoto():
+    camera = picamera.PiCamera()
+    camera.capture('photo.jpg')
+
+
+def main():
+    takephoto()
+
+    credentials = GoogleCredentials.get_application_default()
+    service = discovery.build('vision', 'v1', credentials=credentials)
+
+    with open('photo.jpg', 'rb') as image:
+        image_content = base64.b64encode(iamge.read())
+        service_request = service.images().annotate(body ={
+            'requests': [{
+                'iamge': {
+                    'content': image_content.decode('UTF-8')
+                },
+                'features': [{
+                    'type': 'LABEL_DETECTION',
+                    'maxResults': 10
+                }]
+            }]
+        })
+        response = service_request.execute()
+        print json.dumps(response, indent=4, sort_keys=True)
+
+
+if __name__ == '__main__':
+    main()
